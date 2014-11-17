266117140dbcff4883e0403cb997b425f255d0b2
Audrey <audalogy@gmail.com>
2014-11-17 00:19:07 -0800

    Add bomb icon marker

--- static/js/script.js
+++ static/js/script.js
@@ -120,9 +120,9 @@ function markupMap(data) {
   //
   //
   var bomb_icon = L.icon({
-    iconUrl: 'bomb.png',
+    iconUrl: '/static/img/Bomb.png',
 
-    iconSize:     [38, 95], // size of the icon
+    iconSize:     [30, 30], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
