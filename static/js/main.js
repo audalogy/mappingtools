a26151a14267188b7d4ad8d8d93295d151458046
Vijay Velagapudi <acchi369@gmail.com>
2014-12-02 11:22:56 -0800

    cleaning up code a bit

--- static/js/main.js
+++ static/js/main.js
@@ -20,3 +20,88 @@ $('#nav .navbar-nav li>a').click(function(){
   var posi = $(link).offset().top+20;
   $('body,html').animate({scrollTop:posi},700);
 });
+
+// Accordion control
+$('.controls .btn-primary').click(function(){
+  var active_element = $(".accordion-group").find(".in");
+  if(!active_element){
+    var active_element = $(".accordion-group")[0].find(".accordion-body");
+  }
+  active_element.removeClass("in");
+  active_element.parent().prev().find(".accordion-body").addClass("in");
+});
+
+$('.controls .btn-warning').click(function(){
+  var active_element = $(".accordion-group").find(".in");
+  if($(".accordion-group").find(".in").length != 0) {
+      var active_element = $(".accordion-group").find(".in")
+      active_element.removeClass("in");
+  }
+    else {
+    var active_element = $(".accordion-group").find(".accordion-body")[0];
+  }
+  console.log(active_element)
+  active_element.parent().next().find(".accordion-body").addClass("in");
+});
+
+// Bind click on accordion to event on map
+$(".accordion-group .accordion-heading a").on("click",function(){
+  if($(this).parent().siblings("div").hasClass("accordion-body in collapse")){
+  }else{
+    var markers = [];
+    var marker = {};
+    marker.name = $(this).parent().parent().find("a").text().trim();
+    marker.latlng = $(this).parent().parent().find(".latlong").text();
+    marker.order = $(this).parent().parent().find(".accordion-body").attr('id');
+    markers.push(marker);
+    markupMap(markers);
+  };
+});
+
+// Toggle Map Size
+// Need to be animated... Can perhaps be done through css?
+$('.span12, .span10').toggleClass("span10 span12");
+
+////on click of Next button, go to next order marker
+//$('#nextData').on('click', function() {
+//if (data.length > 0 && minIndex < data.length-1) {
+//
+//  minIndex++
+//  nextIndex = minIndex
+//  //find lat and long of next order marker
+//  var nextlatlng = data[nextIndex].latlng.split(",");
+//  var nextLng = nextlatlng[1];
+//  var nextLat = nextlatlng[0];
+//  var nextMarker = L.marker([nextLat, nextLng],{icon: bomb_icon}).addTo(map);
+//  //add name
+//  var nextName = "Name: " + data[nextIndex].name + "<br />";
+//  //bind popup
+//  nextMarker.bindPopup(nextName);
+//  // open popup
+//  nextMarker.openPopup(nextLat, nextLng);
+//  //zoom to marker
+//  map.setView([nextLat, nextLng], 8, {animation:true});
+//  //}
+//}
+//});
+//
+////on click of Back button, go to last order marker
+//$('#backData').on('click', function() {
+//if (data.length > 0) {
+//  minIndex--
+//  backIndex = minIndex
+//  //find lat and long of back order marker
+//  var backlatlng = data[backIndex].latlng.split(",");
+//  var backLng = backlatlng[1];
+//  var backLat = backlatlng[0];
+//  var backMarker = L.marker([backLat, backLng],{icon: bomb_icon}).addTo(map);
+//  //add name
+//  var backName = "Name: " + data[backIndex].name + "<br />";
+//  //bind popup
+//  backMarker.bindPopup(backName);
+//  //open popup
+//  backMarker.openPopup(backLat, backLng);
+//  //zoom to marker
+//  map.setView([backLat, backLng], 8, {animation: true});
+//}
+//});
--- static/js/map.js
+++ static/js/map.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 
   // Popup Stuff
   var popup = L.popup();
-  getLatestData();
 });
 
-function getLatestData() {
+// Plot all points on page to map
+function plotAllData() {
   var markers = [];
     $(".accordion-group").each(function(ll) {
         marker = {};
@@ -23,9 +23,7 @@ function getLatestData() {
         marker.order = $(this).find(".accordion-body").attr('id');
         markers.push(marker);
     });
-  //console.log("JSON Data: " + JSON.stringify(data[0].marker.length));
   markupMap(markers);
-  // buildTable(data);
 }
 
 function markupMap(data) {
@@ -35,6 +33,12 @@ function markupMap(data) {
   var minIndex = 0;
   //
   // bomb icon
+  var minLong;
+  var minLat;
+  var minOrder = 4;
+  var minMarker;
+  //
+
   var bomb_icon = L.icon({
     iconUrl: '/static/img/Bomb.png',
 
@@ -60,52 +64,4 @@ function markupMap(data) {
     }
     //
   }
-
-  //on click of Next button, go to next order marker
-  $('#nextData').on('click', function() {
-    if (data.length > 0 && minIndex < data.length-1) {
-      
-      minIndex++
-      nextIndex = minIndex  
-      //find lat and long of next order marker
-      var nextlatlng = data[nextIndex].latlng.split(",");
-      var nextLng = nextlatlng[1];
-      var nextLat = nextlatlng[0];
-      var nextMarker = L.marker([nextLat, nextLng],{icon: bomb_icon}).addTo(map);
-      //add name 
-      var nextName = "Name: " + data[nextIndex].name + "<br />";
-      //bind popup
-      nextMarker.bindPopup(nextName);
-      // open popup 
-      nextMarker.openPopup(nextLat, nextLng);  
-      //zoom to marker
-      map.setView([nextLat, nextLng], 8, {animation:true});  
-      //}
-    } 
-  });
-
-  //on click of Back button, go to last order marker
-  $('#backData').on('click', function() {
-   if (data.length > 0) {
-      minIndex--
-      backIndex = minIndex  
-      //find lat and long of back order marker
-      var backlatlng = data[backIndex].latlng.split(",");
-      var backLng = backlatlng[1];
-      var backLat = backlatlng[0];
-      var backMarker = L.marker([backLat, backLng],{icon: bomb_icon}).addTo(map);
-      //add name 
-      var backName = "Name: " + data[backIndex].name + "<br />";
-      //bind popup
-      backMarker.bindPopup(backName);
-      //open popup
-      backMarker.openPopup(backLat, backLng);
-      //zoom to marker
-      map.setView([backLat, backLng], 8, {animation: true});
-    } 
-  });
 }
-
-// Toggle Map Size
-// Need to be animated... Can perhaps be done through css?
-$('.span12, .span10').toggleClass("span10 span12");
