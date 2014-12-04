4feb2d27022afa0ef9839b0ecab3002c0d27024d
Vijay Velagapudi <acchi369@gmail.com>
2014-12-03 17:57:11 -0800

    adding some bad styling

--- app.py
+++ app.py
@@ -19,6 +19,99 @@ def seed_db():
         'created_at': '11-12-2014 21:00:00',
         'updated_by': 'Vijay',
         'updated_at': '11-13-2014 09:00:00',
+        'background_url': '/static/img/blitz.jpg',
+        'marker':
+            [
+                {
+                    'marker_name': 'First hit of the night to Royal Arsenal ',
+                    'marker_type': 'Blue Pin',
+                    'marker_order': 1,
+                    'marker_description': '(Owned by H.M War Department) E.50 A building of 2 floors about 300x200 used as workrooms, machine rooms office and store damaged by explosion',
+                    'latitude': '54.5608864',
+                    'longitude': '-2.2125118',
+                    'include_modern_map': 'True',
+                    'people': [],
+                    'date':
+                        {
+                            'date_type': 'datetime',
+                            'start_date': '09-07-1940 14:55:00',
+                            'end_date': ''
+                        },
+                    'image':
+                        {
+                            'file_name': '',
+                            'caption': ''
+                        },
+                    'audio':
+                        {
+                            'file_name': '',
+                            'caption': ''
+                        }
+                },
+                {
+                    'marker_name': 'Last bomb on the first night of the blitz',
+                    'marker_type': 'Blue Pin',
+                    'marker_order': 3,
+                    'marker_description': '57 buildings damaged by explosion. 4 persons severely injured, extricated by the Brigade and removed to hospital.',
+                    'latitude': '51.51396',
+                    'longitude': '-0.003383',
+                    'include_modern_map': 'True',
+                    'people': [],
+                    'date':
+                        {
+                            'date_type': 'datetime range',
+                            'start_date': '09-07-1940 23:59:00',
+                            'end_date': '09-07-1940 24:00:00'
+                        },
+                    'image':
+                        {
+                            'file_name': '',
+                            'caption': ''
+                        },
+                    'audio':
+                        {
+                            'file_name': '',
+                            'caption': ''
+                        }
+                },
+                {
+                   'marker_name': 'BBC Braodcasting House',
+                   'marker_type': 'Blue Pin',
+                   'marker_order': 2,
+                   'marker_description': 'The BBC Broadcasting House was bombed during an evening broadcast. Radio announcer Bruce Belfrage, paused briefly as the bomb went off and then continued reading the evening news. This was one of several direct hits to the building during the Blitz.',
+                   'latitude': '51.518571',
+                   'longitude': '-0.143811',
+                   'include_modern_map': 'True',
+                   'people': ['Bruce Belfrage'],
+                   'date':
+                       {
+                           'date_type': 'datetime',
+                           'start_date': '10-14-1940 20:00:00',
+                           'end_date': ''
+                       },
+                   'image':
+                       {
+                           'file_name': '',
+                           'caption': ''
+                       },
+                   'audio':
+                       {
+                           'file_name': '',
+                           'caption': ''
+                       }
+                }
+            ]
+        },
+        {
+        'event_name': 'Alexander\'s Conquests',
+        'event_description': 'The Blitz refers to the strategic bombing campaign conducted by the Germans against London and other cities in England from September of 1940 through May of 1941, targeting populated areas, factories and dock yards. The first German attack on London actually occurred by accident.',
+        'event_conclusion': "We hope that you learned something about The Blitz that you didn't know. Come back and see us again.",
+        'event_type': 'Historical',
+        'created_by': 'Vijay',
+        'created_at': '11-12-2014 21:00:00',
+        'updated_by': 'Vijay',
+        'updated_at': '11-13-2014 09:00:00',
+        'background_url': '/static/img/alexander.jpg',
         'marker':
             [
                 {
