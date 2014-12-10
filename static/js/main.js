ee507ebdc3ee8f423b18f8a7391bc1c8bf4ad6b8
Vijay Velagapudi <acchi369@gmail.com>
2014-12-09 21:58:57 -0800

    adding button control jquery

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,19 @@
+// Nav Bar Stuff
+function AdjustHeight() {
+    var height = document.getElementById("topbar").offsetHeight;
+    document.getElementsByClassName("container")[0].style.marginTop = height + 'px';
+}
 
-/* affix the navbar after scroll below header */
-$('#nav').affix({
-      offset: {
-        top: $('header').height()-$('#nav').height()
-      }
-});
+$(document).ready(
+    AdjustHeight()
+);
 
-/* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav' });
-
-/* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
-});
-
-/* smooth scrolling for nav sections */
-$('#nav .navbar-nav li>a').click(function(){
-  var link = $(this).attr('href');
-  var posi = $(link).offset().top+20;
-  $('body,html').animate({scrollTop:posi},700);
+$(window).resize(function() {
+    AdjustHeight()
 });
 
 // Accordion control
+// Back Button
 $('.controls .btn-primary').click(function(){
   var active_element = $(".accordion-group").find(".in");
   if(!active_element){
@@ -31,77 +23,50 @@ $('.controls .btn-primary').click(function(){
   active_element.parent().prev().find(".accordion-body").addClass("in");
 });
 
+// Forward Button
 $('.controls .btn-warning').click(function(){
-  var active_element = $(".accordion-group").find(".in");
   if($(".accordion-group").find(".in").length != 0) {
-      var active_element = $(".accordion-group").find(".in")
+      var active_element = $(".accordion-group").find(".in");
       active_element.removeClass("in");
+      active_element.parent().next().find(".accordion-body").addClass("in");
   }
     else {
-    var active_element = $(".accordion-group").find(".accordion-body")[0];
+        $(".accordion-group").find(".accordion-body").addClass("in");
   }
-  console.log(active_element)
-  active_element.parent().next().find(".accordion-body").addClass("in");
 });
 
-// Bind click on accordion to event on map
-$(".accordion-group .accordion-heading a").on("click",function(){
-  if($(this).parent().siblings("div").hasClass("accordion-body in collapse")){
-  }else{
+// Helper function to add visible markers to map
+function mapCurrentMarkers() {
+  if($(".accordion-group").hasClass(".in")) {
+  }
+  else {
     var markers = [];
     var marker = {};
-    marker.name = $(this).parent().parent().find("a").text().trim();
-    marker.latlng = $(this).parent().parent().find(".latlong").text();
-    marker.order = $(this).parent().parent().find(".accordion-body").attr('id');
+    marker.name = $('.accordion .in').parent().find("a").text().trim();
+    marker.latlng = $('.accordion .in').find(".latlong").text();
+    marker.order = $('.accordion .in').attr('id');
     markers.push(marker);
     markupMap(markers);
   };
+}
+
+// Observe Accordion
+// Plot visible markers on map when accordion is opened
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    mapCurrentMarkers()
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe($('.accordion')[0], {
+      subtree: true,
+      attributes: true
+      //...
 });
 
 // Toggle Map Size
 // Need to be animated... Can perhaps be done through css?
-$('.span12, .span10').toggleClass("span10 span12");
-
-//on click of Next button, go to next order marker
-$('#nextData').on('click', function() {
-if (data.length > 0 && minIndex < data.length-1) {
-
-  minIndex++
-  nextIndex = minIndex
-  //find lat and long of next order marker
-  var nextlatlng = data[nextIndex].latlng.split(",");
-  var nextLng = nextlatlng[1];
-  var nextLat = nextlatlng[0];
-  var nextMarker = L.marker([nextLat, nextLng],{icon: bomb_icon}).addTo(map);
-  //add name
-  var nextName = "Name: " + data[nextIndex].name + "<br />";
-  //bind popup
-  nextMarker.bindPopup(nextName);
-  // open popup
-  nextMarker.openPopup(nextLat, nextLng);
-  //zoom to marker
-  map.setView([nextLat, nextLng], 8, {animation:true});
-  //}
-}
-});
-
-//on click of Back button, go to last order marker
-$('#backData').on('click', function() {
-if (data.length > 0) {
-  minIndex--
-  backIndex = minIndex
-  //find lat and long of back order marker
-  var backlatlng = data[backIndex].latlng.split(",");
-  var backLng = backlatlng[1];
-  var backLat = backlatlng[0];
-  var backMarker = L.marker([backLat, backLng],{icon: bomb_icon}).addTo(map);
-  //add name
-  var backName = "Name: " + data[backIndex].name + "<br />";
-  //bind popup
-  backMarker.bindPopup(backName);
-  //open popup
-  backMarker.openPopup(backLat, backLng);
-  //zoom to marker
-  map.setView([backLat, backLng], 8, {animation: true});
-}
-});
+$('.col-md-8, .col-md-2').toggleClass("col-md-8 col-md-2");
