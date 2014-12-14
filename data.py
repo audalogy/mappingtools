fa9357d9dfe1707b21c7bf9b4e6b018581a62cf2
Vijay Velagapudi <acchi369@gmail.com>
2014-12-13 22:54:12 -0700

    fixing some data structure bugs

--- data.py
+++ data.py
@@ -291,10 +291,6 @@ def seed():
                             {
                                 'order':1,
                                 'file_path':'/static/event_images/royal_arsenal1.jpg',
-                            },
-                            {
-                                'order':2,
-                                'file_path':'/static/event_images/royal_arsenal2.jpg',
                             }
                         ],
                     'date':
@@ -321,6 +317,10 @@ def seed():
                     'people': [],
                     'marker_images':
                         [
+                            {
+                                'order':2,
+                                'file_path':'/static/event_images/royal_arsenal2.jpg',
+                            }
 
                         ],
                     'date':
@@ -360,7 +360,7 @@ def seed():
                 {
                    'marker_name': 'BBC Broadcasting House',
                    'marker_type': 'Blue Pin',
-                   'marker_order': 4,
+                   'marker_order': 5,
                    'marker_description': 'The BBC Broadcasting House was bombed during an evening broadcast. Radio announcer Bruce Belfrage, paused briefly as the bomb went off and then continued reading the evening news. This was one of several direct hits to the building during the Blitz.',
                    'latitude': '51.518571',
                    'longitude': '-0.143811',
